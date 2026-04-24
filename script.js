b9dd8b8da66b807c8d7ac44cc5af759cac36aabe
Ayden <barrowa2555@gmail.com>
2026-04-24 01:32:13 -0500

    Add script.js with JavaScript logic for the game

---
+++ script.js
@@ -0,0 +1,14 @@
+// JavaScript logic for the Clash game
+
+// Example function to start the game
+function startGame() {
+    console.log('Game started!');
+}
+
+// Example function to end the game
+function endGame() {
+    console.log('Game over!');
+}
+
+// Call startGame when the script loads
+startGame();
